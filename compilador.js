45ef041345bb9ca31c08e2c5763ede2b21d98a3f
Matheus Gabriel França <94320912+Matheusfr8@users.noreply.github.com>
2022-02-15 21:16:34 -0300

    Add files via upload

---
+++ compilador.js
@@ -0,0 +1,28 @@
+"use strict";
+let canal = "gaveta";
+let inscritos = 610234;
+//canal = inscritos
+console.log(`canal = ${canal}`);
+nome = "pedro";
+console.log(`nome = ${nome}`);
+(function () {
+    let nome = "Ana";
+})();
+function soma(a, b) {
+    return a + b;
+}
+let qualquercoisa;
+qualquercoisa = 12;
+qualquercoisa = "asv";
+function saudar(isManha, horas) {
+    let a = 1;
+    let saudacao;
+    if (isManha) {
+        saudacao = "bundia";
+    }
+    else {
+        saudacao = "aoba";
+        return saudacao;
+    }
+}
+//# sourceMappingURL=compilador.js.map
